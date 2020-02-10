58123c547597189641234e8539ab85ea632e9dba
RobZinke <zink.oxide81@gmail.com>
2020-02-10 10:39:20 -0800

    Deleted old computeMisclosure script

--- Closure/computeMisclosure.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# Compute the cumulative phase change for 
-#  different pairs of interferograms
-# 
-# Rob Zinke 2019
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
